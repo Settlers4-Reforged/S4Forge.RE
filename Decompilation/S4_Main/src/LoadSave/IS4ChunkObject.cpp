0b0c17ab5a088254f2afdb1272ef924fa6f9a70a
WizzardMaker <wizzardmaker@gmail.com>
2026-01-19 22:45:30 +0100

    added IS4ChunkObject

---
+++ Decompilation/S4_Main/src/LoadSave/IS4ChunkObject.cpp
@@ -0,0 +1,3 @@
+#include "IS4ChunkObject.h"
+
+// All default implementations or pure virtual functions
---
+++ Decompilation/S4_Main/src/LoadSave/IS4ChunkObject.h
@@ -0,0 +1,23 @@
+#ifndef IS4CHUNKOBJECT_H
+#define IS4CHUNKOBJECT_H
+
+#include "defines.h"
+
+class IS4ChunkObject {
+public:
+    // address=[0x1310290]
+    IS4ChunkObject(void) = default;
+
+    // address=[0x14600c0]
+    IS4ChunkObject(class IS4ChunkObject const & a2) = default;
+
+    // address=[0x1486c50]
+    class IS4ChunkObject &  operator=(class IS4ChunkObject const & a2) = default;
+
+    virtual void Load(class IS4Chunk &) = 0;
+    virtual void Save(class IS4Chunk &) = 0;
+    virtual void Clear(void) = 0;
+};
+
+
+#endif // IS4CHUNKOBJECT_H
