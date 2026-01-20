edc869360539b3fbb8e856a9eabc8072c6b712ef
WizzardMaker <wizzardmaker@gmail.com>
2026-01-20 18:41:49 +0100

    added CEvn_Event

---
+++ Decompilation/S4_Main/src/Logic/Events/CEvn_Event.h
@@ -0,0 +1,43 @@
+#ifndef CEVN_EVENT_H
+#define CEVN_EVENT_H
+
+#include "defines.h"
+
+#include "../../LoadSave/IS4ChunkObject.h"
+
+struct SEventStruct
+{
+  int m_iEventId;
+  int m_wParam;
+  int m_lParam;
+  int m_iTick;
+  BYTE m_iFlags;
+  BYTE unk_15; // probably padding
+  BYTE unk_16; // probably padding
+  BYTE unk_17; // probably padding
+};
+
+class CEvn_Event : public SEventStruct, public IS4ChunkObject {
+public:
+    // address=[0x130e450]
+    virtual  ~CEvn_Event(void);
+
+    // address=[0x1334dc0]
+     CEvn_Event(unsigned int a2, unsigned int a3, unsigned int a4, unsigned char a5);
+
+    // address=[0x1352be0]
+    virtual void  Load(class IS4Chunk & a2);
+
+    // address=[0x1352bf0]
+    virtual void  Save(class IS4Chunk & a2);
+
+    // address=[0x145ff30]
+     CEvn_Event(class CEvn_Event const & a2);
+
+    // address=[0x1486b30]
+    class CEvn_Event &  operator=(class CEvn_Event const & a2);
+
+};
+
+
+#endif // CEVN_EVENT_H
