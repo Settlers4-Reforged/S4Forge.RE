bfcddac67cef2ebb44d7e7f5c863f6c339ef3fa2
WizzardMaker <wizzardmaker@gmail.com>
2026-03-14 20:22:32 +0100

    added CAIAgentEvaluation

---
+++ Decompilation/S4_Main/src/AI/CAIAgentEvaluation.cpp
@@ -0,0 +1,57 @@
+#include "CAIAgentEvaluation.h"
+
+#include <Debug/IMessageTracer.h>
+
+#include "CAIPlayerEvaluations.h"
+#include "AI/IAIEnvironment.h"
+#include "CBB/CBBSupport.h"
+
+// Definitions for class CAIAgentEvaluation
+
+// address=[0x1303a40]
+// Decompiled from CAIAgentEvaluation *__thiscall CAIAgentEvaluation::CAIAgentEvaluation(  CAIAgentEvaluation *this,  struct CAIPlayerEvaluations *a2)
+CAIAgentEvaluation::CAIAgentEvaluation(class CAIPlayerEvaluations &a2) : CAIAgent("player evaluation"), m_pAIPlayerEvaluations(a2) {
+  this->m_iCurrentPlayerId = 1;
+}
+
+
+// address=[0x1303a80]
+// Decompiled from unsigned int __thiscall CAIAgentEvaluation::Execute(CAIAgentEvaluation *this, unsigned int a2, unsigned int a3)
+unsigned int CAIAgentEvaluation::Execute(unsigned int a2, unsigned int a3) {
+  if(this->m_iCurrentPlayerId < 1
+     && BBSupportDbgReport(2, "AI\\AI_AgentsEx.cpp", 43, "m_iCurrentPlayerId >= PLAYER_FIRST") == 1) {
+    __debugbreak();
+  }
+  if(this->m_iCurrentPlayerId > 8
+     && BBSupportDbgReport(2, "AI\\AI_AgentsEx.cpp", 44, "m_iCurrentPlayerId <= PLAYER_LAST") == 1) {
+    __debugbreak();
+  }
+  ++this->m_iCurrentPlayerId;
+  if(this->m_iCurrentPlayerId > IAIEnvironment::AlliancesLastPlayerId())
+    this->m_iCurrentPlayerId = 1;
+  if(this->m_iCurrentPlayerId < 1
+     && BBSupportDbgReport(2, "AI\\AI_AgentsEx.cpp", 55, "m_iCurrentPlayerId >= PLAYER_FIRST") == 1) {
+    __debugbreak();
+  }
+  if(this->m_iCurrentPlayerId > 8
+     && BBSupportDbgReport(2, "AI\\AI_AgentsEx.cpp", 56, "m_iCurrentPlayerId <= PLAYER_LAST") == 1) {
+    __debugbreak();
+  }
+  this->m_pAIPlayerEvaluations.EvaluatePlayer(this->m_iCurrentPlayerId);
+  IMessageTracer::PushFormatedInts(g_pMsgTracer2, aEvaluatingPlay, this->m_iCurrentPlayerId);
+  return CAIAgent::ExecuteResult(0, 0);
+}
+
+
+// address=[0x1303be0]
+// Decompiled from void __thiscall CAIAgentEvaluation::~CAIAgentEvaluation(CAIAgentEvaluation *this)
+CAIAgentEvaluation::~CAIAgentEvaluation(void) = default;
+
+
+// address=[0x1314030]
+// Decompiled from void __thiscall CAIAgentEvaluation::Init(CAIAgentEvaluation *this)
+void CAIAgentEvaluation::Init(void) {
+  this->m_iCurrentPlayerId = 1;
+}
+
+
---
+++ Decompilation/S4_Main/src/AI/CAIAgentEvaluation.h
@@ -0,0 +1,29 @@
+#ifndef CAIAGENTEVALUATION_H
+#define CAIAGENTEVALUATION_H
+
+#include "defines.h"
+
+#include "CAIAgent.h"
+
+class CAIAgentEvaluation : public CAIAgent {
+public:
+    // address=[0x1303a40]
+    CAIAgentEvaluation(class CAIPlayerEvaluations &a2);
+
+    // address=[0x1303a80]
+    virtual unsigned int Execute(unsigned int a2, unsigned int a3);
+
+    // address=[0x1303be0]
+    virtual ~CAIAgentEvaluation(void);
+
+    // address=[0x1314030]
+    void Init(void);
+
+    // Type information members
+public:
+    int                   m_iCurrentPlayerId;
+    CAIPlayerEvaluations &m_pAIPlayerEvaluations;
+};
+
+
+#endif // CAIAGENTEVALUATION_H
