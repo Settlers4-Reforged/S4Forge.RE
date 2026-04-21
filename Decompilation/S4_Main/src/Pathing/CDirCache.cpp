7759eb905a0baa4803cd96447a160f11b6f54337
WizzardMaker <wizzardmaker@gmail.com>
2026-04-21 16:24:29 +0200

    added CDirCache

---
+++ Decompilation/S4_Main/src/Pathing/CDirCache.cpp
@@ -0,0 +1,67 @@
+#include "CDirCache.h"
+
+#include "CBB/CBBSupport.h"
+
+// Definitions for class CDirCache
+
+// address=[0x15d6690]
+// Decompiled from void __thiscall CDirCache::Init(CDirCache *this)
+void CDirCache::Init(void) {
+  this->m_iCount = 0;
+  this->m_iPointer = 15;
+}
+
+
+// address=[0x15d69a0]
+// Decompiled from void __thiscall CDirCache::PushBack(CDirCache *this, BYTE a2)
+void CDirCache::PushBack(int a2) {
+  this->m_iCount += this->m_iCount < 16u;
+  this->m_iPointer = CDirCache::IncWrap(this->m_iPointer);
+  this->m_aValues[this->m_iPointer] = a2;
+}
+
+
+// address=[0x15faff0]
+// Decompiled from int __thiscall CDirCache::operator[](CDirCache *this, char a2)
+int CDirCache::operator[](int a2) {
+  return this->m_aValues[(this->m_iPointer - a2) & 0xF];
+}
+
+
+// address=[0x15fb1a0]
+// Decompiled from int __thiscall CDirCache::Back(CDirCache *this)
+int CDirCache::Back(void) const {
+  return this->m_iPointer[this->m_iPointer[0] + 1];
+}
+
+
+// address=[0x15fb1c0]
+// Decompiled from int __thiscall CDirCache::Count(CDirCache *this)
+int CDirCache::Count(void) const {
+  return this->m_iCount;
+}
+
+
+// address=[0x15fb520]
+// Decompiled from void __thiscall CDirCache::PopBack(CDirCache *this)
+void CDirCache::PopBack(void) {
+  BB_ASSERT(this->m_iCount > 0)
+  --this->m_iCount;
+  this->m_iPointer = CDirCache::DecWrap(this->m_iPointer);
+}
+
+
+// address=[0x15d6600]
+// Decompiled from int __cdecl CDirCache::IncWrap(char a1)
+unsigned char __cdecl CDirCache::IncWrap(unsigned char a1) {
+  return (a1 + 1) & 0xF;
+}
+
+
+// address=[0x15fb1e0]
+// Decompiled from int __cdecl CDirCache::DecWrap(char a1)
+unsigned char __cdecl CDirCache::DecWrap(unsigned char a1) {
+  return (a1 - 1) & 0xF;
+}
+
+
---
+++ Decompilation/S4_Main/src/Pathing/CDirCache.h
@@ -0,0 +1,41 @@
+#ifndef CDIRCACHE_H
+#define CDIRCACHE_H
+
+#include "defines.h"
+
+class CDirCache {
+public:
+    // address=[0x15d6690]
+    void Init(void);
+
+    // address=[0x15d69a0]
+    void PushBack(int a2);
+
+    // address=[0x15faff0]
+    int operator[](int a2);
+
+    // address=[0x15fb1a0]
+    int Back(void) const;
+
+    // address=[0x15fb1c0]
+    int Count(void) const;
+
+    // address=[0x15fb520]
+    void PopBack(void);
+
+private:
+    // address=[0x15d6600]
+    static unsigned char __cdecl IncWrap(unsigned char a1);
+
+    // address=[0x15fb1e0]
+    static unsigned char __cdecl DecWrap(unsigned char a1);
+
+    // Type information members
+public:
+    BYTE m_iCount;
+    BYTE m_iPointer;
+    BYTE m_aValues[16];
+};
+
+
+#endif // CDIRCACHE_H
