f50c3882e19f0e8bf1691785c313273e4f762e17
WizzardMaker <wizzardmaker@gmail.com>
2026-04-23 16:11:05 +0200

    added IAnimatedEntity

---
+++ Decompilation/S4_Main/src/MapObjects/IAnimatedEntity.cpp
@@ -0,0 +1,328 @@
+#include "IAnimatedEntity.h"
+
+#include <iostream>
+
+#include "Logic/Events/CEntityEvent.h"
+#include "CMapObjectMgr.h"
+#include "CBB/CS4InvalidMapException.h"
+#include "CBB/CBBSupport.h"
+#include "Debug/IMessageTracer.h"
+
+// Definitions for class IAnimatedEntity
+
+// address=[0x1307150]
+// Decompiled from int __thiscall IAnimatedEntity::Next(IAnimatedEntity *this)
+int IAnimatedEntity::Next(void) const {
+  return this->m_wNextEntity;
+}
+
+
+// address=[0x1351b10]
+// Decompiled from int __thiscall IAnimatedEntity::Frame(IAnimatedEntity *this)
+int IAnimatedEntity::Frame(void) const {
+  return this->m_cFrame;
+}
+
+
+// address=[0x1351b30]
+// Decompiled from int __thiscall IAnimatedEntity::GetLastLogicUpdateTick(IAnimatedEntity *this)
+int IAnimatedEntity::GetLastLogicUpdateTick(void) const {
+  return this->m_uLastLogicUpdate;
+}
+
+
+// address=[0x1439c70]
+// Decompiled from int __thiscall IAnimatedEntity::Previous(IAnimatedEntity *this)
+int IAnimatedEntity::Previous(void) const {
+  return this->m_wPrevEntity;
+}
+
+
+// address=[0x1439e10]
+// Decompiled from void __thiscall IAnimatedEntity::SetNext(IAnimatedEntity *this, int _id)
+void IAnimatedEntity::SetNext(int _id) {
+  BB_ASSERT(ID() != _id)
+  this->m_wNextEntity = _id;
+}
+
+
+// address=[0x1439eb0]
+// Decompiled from void __thiscall IAnimatedEntity::SetPrevious(IAnimatedEntity *this, WORD a2)
+void IAnimatedEntity::SetPrevious(int a2) {
+  this->m_wPrevEntity = a2;
+}
+
+
+// address=[0x14d87d0]
+// Decompiled from void __thiscall IAnimatedEntity::SetLastUpdateTick(IAnimatedEntity *this, DWORD a2)
+void IAnimatedEntity::SetLastUpdateTick(unsigned int a2) {
+  this->m_uLastUpdateTick = a2;
+}
+
+
+// address=[0x14e31a0]
+// Decompiled from int __thiscall IAnimatedEntity::RegisterForLogicUpdate(IAnimatedEntity *this, int _iDeltaTicks)
+int IAnimatedEntity::RegisterForLogicUpdate(int _iDeltaTicks) {
+  int v3 = g_pMapObjectMgr->RegisterForLogicUpdate(_iDeltaTicks, IEntity::ID());
+  return IAnimatedEntity::SetLastLogicUpdate(v3);
+}
+
+
+// address=[0x14e31d0]
+// Decompiled from void __thiscall IAnimatedEntity::UnRegisterFromLogicUpdate(IAnimatedEntity *this)
+void IAnimatedEntity::UnRegisterFromLogicUpdate(void) {
+  // eax
+  // [esp-4h] [ebp-8h]
+
+  int          iEntityId = IEntity::ID();
+  unsigned int iLastLogicUpdateTick = IAnimatedEntity::GetLastLogicUpdateTick();
+  g_pMapObjectMgr->UnRegisterFromLogicUpdate(iLastLogicUpdateTick, iEntityId);
+  IAnimatedEntity::SetLastLogicUpdate(-1);
+}
+
+
+// address=[0x14e3210]
+// Decompiled from void __thiscall IAnimatedEntity::SetEvent(IAnimatedEntity *this, const struct CEntityEvent *a1)
+void IAnimatedEntity::SetEvent(CEntityEvent const &a1) {
+  // eax
+  // [esp-8h] [ebp-Ch]
+  // [esp-4h] [ebp-8h]
+
+  DWORD m_iType = a1.m_iType;
+  DWORD m_iEvent = a1.m_iEvent;
+  DWORD v2 = IEntity::EntityId();
+  g_pMsgTracer->PushFormatedInts("SetEvent(): id %u, event %u, type %u", v2, m_iEvent, m_iType);
+  if(!IEntity::FlagBits(EntityFlag_Registered))
+    IAnimatedEntity::RegisterForLogicUpdate(1);
+  this->m_iEventQueue.push_back(a1);
+}
+
+
+// address=[0x14e3270]
+// Decompiled from void __thiscall IAnimatedEntity::ClearAllQueuedEvents(IAnimatedEntity *this)
+void IAnimatedEntity::ClearAllQueuedEvents(void) {
+  this->m_iEventQueue.clear();
+}
+
+
+// address=[0x14e3290]
+// Decompiled from void __thiscall IAnimatedEntity::BoxSelection(IAnimatedEntity *this)
+void IAnimatedEntity::BoxSelection(void) {
+  if(IEntity::OwnerId() == g_cInputProcessor.m_iSelectionLocalPlayerId) {
+    int v6 = g_cInputProcessor.StrictSelection();
+    int mask = 0xFF0000;
+    if(v6 == 1) {
+      mask = 0xFFFF00;
+    } else if(v6 == 2) {
+      mask = 0xFFFFFF;
+    }
+    int EntitySelectionType = g_cInputProcessor.GetEntitySelectionType(this);
+    int SelectionType = g_cInputProcessor.GetSelectionType();
+    if(IEntity::FlagBits(EntityFlag_Selectable) || IEntity::ObjType() == 8) {
+      bool v10 = 0;
+      if(v6) {
+        v10 = (mask & EntitySelectionType) == (mask & SelectionType);
+      } else if((EntitySelectionType & 0xFF0000) <= (SelectionType & 0xFF0000)) {
+        if((EntitySelectionType & 0xFF0000) == (SelectionType & 0xFF0000))
+          v10 = (EntitySelectionType & 0xFF00) == (SelectionType & 0xFF00);
+      } else {
+        v10 = 1;
+      }
+      if(v10) {
+        int xScreenOffset = -1;
+        int yScreenOffset = -1;
+        if(IGfxEngine::GetScreenOffsetsByMapIndices(IEntity::X(), IEntity::Y(), &xScreenOffset, &yScreenOffset)) {
+          if(g_cInputProcessor.m_iSelectionXStart <= xScreenOffset
+             && g_cInputProcessor.m_iSelectionXEnd >= xScreenOffset
+             && g_cInputProcessor.m_iSelectionYStart <= yScreenOffset
+             && g_cInputProcessor.m_iSelectionYEnd >= yScreenOffset) {
+            if(!g_cInputProcessor.BoxSelectAllSettler())
+              g_cInputProcessor.SetSelectionType(EntitySelectionType);
+            g_cInputProcessor.NewCandidate(IEntity::EntityId());
+          }
+        }
+      }
+    }
+  }
+}
+
+
+// address=[0x14e46c0]
+// Decompiled from int __thiscall IAnimatedEntity::~IAnimatedEntity(IAnimatedEntity *this)
+IAnimatedEntity::~IAnimatedEntity(void) = default;
+
+
+// address=[0x14eb000]
+// Decompiled from IAnimatedEntity *__thiscall IAnimatedEntity::IAnimatedEntity(IAnimatedEntity *this, int id)
+IAnimatedEntity::IAnimatedEntity(int id) : IEntity(id), m_iEventQueue() {
+  this->m_cFrame = 0;
+  this->m_cAttackerPlayerId = 0;
+  this->m_wJobPart = 0;
+  this->m_wPrevEntity = 0;
+  this->m_wNextEntity = 0;
+  this->m_uLastUpdateTick = 0;
+  this->m_uLastLogicUpdate = -1;
+}
+
+
+// address=[0x14eb2a0]
+// Decompiled from int __thiscall IAnimatedEntity::AttackerPlayerId(IAnimatedEntity *this)
+int IAnimatedEntity::AttackerPlayerId(void) const {
+  return this->m_cAttackerPlayerId;
+}
+
+
+// address=[0x1501170]
+// Decompiled from DWORD __thiscall IAnimatedEntity::LastUpdateTick(IAnimatedEntity *this)
+unsigned int IAnimatedEntity::LastUpdateTick(void) const {
+  return this->m_uLastUpdateTick;
+}
+
+
+// address=[0x1548370]
+// Decompiled from int __thiscall IAnimatedEntity::JobPart(IAnimatedEntity *this)
+int IAnimatedEntity::JobPart(void) const {
+  return this->m_wJobPart;
+}
+
+
+// address=[0x15639c0]
+// Decompiled from void __thiscall IAnimatedEntity::SetFrame(IAnimatedEntity *this, BYTE a2)
+void IAnimatedEntity::SetFrame(int a2) {
+  this->m_cFrame = a2;
+}
+
+
+// address=[0x15670a0]
+// Decompiled from void __thiscall IAnimatedEntity::EventQueueEmpty(IAnimatedEntity *this)
+bool IAnimatedEntity::EventQueueEmpty(void) const {
+  std::vector<CEntityEvent>::empty(&this->m_iEventQueue);
+}
+
+
+// address=[0x1567140]
+// Decompiled from void __thiscall IAnimatedEntity::SetJobPart(IAnimatedEntity *this, WORD a2)
+void IAnimatedEntity::SetJobPart(int a2) {
+  this->m_wJobPart = a2;
+}
+
+
+// address=[0x14e3460]
+// Decompiled from IAnimatedEntity *__thiscall IAnimatedEntity::IAnimatedEntity(IAnimatedEntity *this, struct std::istream *a1)
+IAnimatedEntity::IAnimatedEntity(std::istream &_rStream) : IEntity(_rStream), m_iEventQueue() {
+  int fileVersion; // [esp+Ch] [ebp-1Ch] MAPDST BYREF
+  _rStream >> fileVersion;
+  if(fileVersion != 1) {
+    BBSupportTracePrintF(3, "load output defect IAnimatedEntity::IAnimatedEntity");
+    throw CS4InvalidMapException{};
+  }
+  _rStream >> this->m_cFrame;
+  _rStream >> this->m_cAttackerPlayerId;
+  _rStream >> this->m_wJobPart;
+  _rStream >> this->m_wPrevEntity;
+  _rStream >> this->m_wNextEntity;
+  _rStream >> this->m_uLastUpdateTick;
+  _rStream >> this->m_uLastLogicUpdate;
+  unsigned int iQueueSize; // [esp+4h] [ebp-24h] BYREF
+  _rStream >> iQueueSize;
+
+  // Ensure loading compatibility from the binary
+  static_assert(sizeof(m_cFrame) == sizeof(unsigned char));
+  static_assert(sizeof(m_cAttackerPlayerId) == sizeof(unsigned char));
+  static_assert(sizeof(m_wJobPart) == sizeof(unsigned short));
+  static_assert(sizeof(m_wPrevEntity) == sizeof(unsigned short));
+  static_assert(sizeof(m_wNextEntity) == sizeof(unsigned short));
+  static_assert(sizeof(m_uLastUpdateTick) == sizeof(unsigned int));
+  static_assert(sizeof(m_uLastLogicUpdate) == sizeof(int));
+  static_assert(sizeof(iQueueSize) == sizeof(unsigned int));
+
+  for(unsigned int i = 0; i < iQueueSize; ++i) {
+    const CEntityEvent *v2 = CEntityEvent::Load(_rStream);
+    this->m_iEventQueue.push_back(*v2);
+  }
+}
+
+
+// address=[0x14e35f0]
+// Decompiled from int __thiscall IAnimatedEntity::Store(IAnimatedEntity *this, struct std::ostream *a2)
+void IAnimatedEntity::Store(std::ostream &_rStream) {
+  _BYTE v3[12]; // [esp+4h] [ebp-40h] BYREF
+  _BYTE v4[12]; // [esp+10h] [ebp-34h] BYREF
+  // [esp+1Ch] [ebp-28h]
+  // [esp+20h] [ebp-24h]
+
+  int iFileVersion; // [esp+28h] [ebp-1Ch] BYREF
+  // [esp+2Ch] [ebp-18h]
+  // [esp+37h] [ebp-Dh]
+  // [esp+40h] [ebp-4h]
+
+  IEntity::Store(_rStream);
+  iFileVersion = 1;
+  _rStream << iFileVersion;
+  _rStream << this->m_cFrame;
+  _rStream << this->m_cAttackerPlayerId;
+  _rStream << this->m_wJobPart;
+  _rStream << this->m_wPrevEntity;
+  _rStream << this->m_wNextEntity;
+  _rStream << this->m_uLastUpdateTick;
+  _rStream << this->m_uLastLogicUpdate;
+  unsigned int iQueueSize = this->m_iEventQueue.size();
+  _rStream << iQueueSize;
+
+  static_assert(sizeof(iFileVersion) == sizeof(unsigned int));
+  static_assert(sizeof(m_cFrame) == sizeof(unsigned char));
+  static_assert(sizeof(m_cAttackerPlayerId) == sizeof(unsigned char));
+  static_assert(sizeof(m_wJobPart) == sizeof(unsigned short));
+  static_assert(sizeof(m_wPrevEntity) == sizeof(unsigned short));
+  static_assert(sizeof(m_wNextEntity) == sizeof(unsigned short));
+  static_assert(sizeof(m_uLastUpdateTick) == sizeof(unsigned int));
+  static_assert(sizeof(m_uLastLogicUpdate) == sizeof(int));
+  static_assert(sizeof(iQueueSize) == sizeof(unsigned int));
+
+  for(auto &rEvent: this->m_iEventQueue) {
+    rEvent.Store(_rStream);
+  }
+}
+
+
+// address=[0x14e3780]
+// Decompiled from bool __thiscall IAnimatedEntity::ProcessAllEvents(IAnimatedEntity *this)
+bool IAnimatedEntity::ProcessAllEvents(void) {
+  //NOTE: This was switched from a iterator with erase for every step to just iterating through it and then clearing
+  //TODO: check for ConvertEventIntoGoal side effects that may have been lost with this change
+
+  bool bFlagsSet = false;
+  for(auto &rEvent: this->m_iEventQueue) {
+    this->m_iFlags &= ~0x80000000;
+    g_pMsgTracer->PushFormatedInts(
+      "ConvertEventIntoGoal(): event %u, type %u, data %u, %u, %u",
+      rEvent.m_iEvent,
+      rEvent.m_iType,
+      rEvent.m_iDataA,
+      rEvent.m_iDataB,
+      rEvent.m_iDataC);
+    this->ConvertEventIntoGoal(&rEvent);
+
+    bFlagsSet = this->m_iFlags >= 0;
+  }
+  this->m_iEventQueue.clear();
+
+  return bFlagsSet;
+}
+
+
+// address=[0x14e4ae0]
+// Decompiled from void __stdcall IAnimatedEntity::ConvertEventIntoGoal(int a1)
+void IAnimatedEntity::ConvertEventIntoGoal(class CEntityEvent *a1) {
+  ;
+}
+
+
+// address=[0x14e4b90]
+// Decompiled from int __thiscall IAnimatedEntity::SetLastLogicUpdate(IAnimatedEntity *this, int a2)
+int IAnimatedEntity::SetLastLogicUpdate(int a2) {
+  this->m_uLastLogicUpdate = a2;
+  return a2;
+}
+
+
---
+++ Decompilation/S4_Main/src/MapObjects/IAnimatedEntity.h
@@ -0,0 +1,98 @@
+#ifndef IANIMATEDENTITY_H
+#define IANIMATEDENTITY_H
+
+#include "defines.h"
+#include "IEntity.h"
+
+class IAnimatedEntity : public IEntity {
+public:
+    // address=[0x1307150]
+    int Next(void) const;
+
+    // address=[0x1351b10]
+    int Frame(void) const;
+
+    // address=[0x1351b30]
+    int GetLastLogicUpdateTick(void) const;
+
+    // address=[0x1439c70]
+    int Previous(void) const;
+
+    // address=[0x1439e10]
+    void SetNext(int _id);
+
+    // address=[0x1439eb0]
+    void SetPrevious(int a2);
+
+    // address=[0x14d87d0]
+    void SetLastUpdateTick(unsigned int a2);
+
+    // address=[0x14e31a0]
+    int RegisterForLogicUpdate(int _iDeltaTicks);
+
+    // address=[0x14e31d0]
+    void UnRegisterFromLogicUpdate(void);
+
+    // address=[0x14e3210]
+    virtual void SetEvent(class CEntityEvent const &a1);
+
+    // address=[0x14e3270]
+    virtual void ClearAllQueuedEvents(void);
+
+    // address=[0x14e3290]
+    void BoxSelection(void);
+
+    // address=[0x14e46c0]
+    virtual ~IAnimatedEntity(void);
+
+    // address=[0x14eb000]
+    IAnimatedEntity(int id);
+
+    // address=[0x14eb2a0]
+    int AttackerPlayerId(void) const;
+
+    // address=[0x1501170]
+    unsigned int LastUpdateTick(void) const;
+
+    // address=[0x1548370]
+    int JobPart(void) const;
+
+    // address=[0x15639c0]
+    void SetFrame(int a2);
+
+    // address=[0x15670a0]
+    bool EventQueueEmpty(void) const;
+
+    // address=[0x1567140]
+    void SetJobPart(int a2);
+
+protected:
+    // address=[0x14e3460]
+    IAnimatedEntity(std::istream &_rStream);
+
+    // address=[0x14e35f0]
+    virtual void Store(std::ostream &_rStream);
+
+    // address=[0x14e3780]
+    bool ProcessAllEvents(void);
+
+    // address=[0x14e4ae0]
+    virtual void ConvertEventIntoGoal(class CEntityEvent *a1);
+
+    // address=[0x14e4b90]
+    int SetLastLogicUpdate(int a2);
+
+    // Type information members
+public:
+    BYTE                      m_cFrame;
+    BYTE                      m_cAttackerPlayerId;
+    WORD                      m_wJobPart;
+    WORD                      m_wPrevEntity;
+    WORD                      m_wNextEntity;
+    DWORD                     m_uLastUpdateTick;
+    int                       m_uLastLogicUpdate;
+    std::vector<CEntityEvent> m_iEventQueue;
+};
+
+
+#endif // IANIMATEDENTITY_H
