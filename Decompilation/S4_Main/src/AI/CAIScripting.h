82b5c5b4a57d860bdb154233597ad80e906b6234
WizzardMaker <wizzardmaker@gmail.com>
2026-07-10 00:01:58 +0200

    fixed CAIScripting virtual functions

--- Decompilation/S4_Main/src/AI/CAIScripting.h
+++ Decompilation/S4_Main/src/AI/CAIScripting.h
@@ -6,6 +6,18 @@
 
 class CAIScripting : public IAIScripting {
   public:
+    // address=[0x131d980]
+    virtual void Init(void);
+
+    // address=[0x131db20]
+    virtual void Load(class IS4Chunk &a2);
+
+    // address=[0x131db30]
+    virtual void Save(class IS4Chunk &a2);
+
+    // address=[0x131db40]
+    virtual unsigned int Adler32(unsigned int a2);
+
     // address=[0x131e8c0]
     CAIScripting(void);
 
